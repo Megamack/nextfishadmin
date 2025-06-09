b497e099941df154de5538a54effcb7f13097394
Megamack <markus.sallinen@float.fi>
2025-06-09 23:59:24 +0300

    DEV / just stuff

--- src/auth.ts
+++ src/auth.ts
@@ -30,7 +30,12 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
       },
       authorize: async (credentials) => {
         // Dummy user example
-        const user = { name: "Markus", emai: "markus@gofish.fi" };
+        const user = {
+          name: "Markus",
+          email: "markus@gofish.fi",
+          coverImage: "url.png",
+        };
+
         if (!user) throw new Error("Invalid credentials.");
         return user;
       },
---
+++ src/types/next-auth.d.ts
@@ -0,0 +1,17 @@
+// next-auth.d.ts
+import NextAuth from "next-auth";
+
+declare module "next-auth" {
+  interface User {
+    coverImage?: string | null; // Add your custom property here
+  }
+
+  interface Session {
+    user: {
+      name?: string | null;
+      email?: string | null;
+      image?: string | null;
+      coverImage?: string | null; // Add here as well
+    };
+  }
+}
